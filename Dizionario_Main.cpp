48d991de78a00fc43c026d8e51d0b2413283e170
GianGiusto02 <g.giusto5@studenti.uniba.it>
2024-12-06 12:30:19 +0100

    Dizionario_Main.cpp

--- Dizionario_Main.cpp
+++ Dizionario_Main.cpp
@@ -1,17 +1,17 @@
 
 int main() {
-    DizionarioEsteso<string, string> dict;
+    DizionarioEsteso<string, string> diz;
 
     cout << "Inserimenti:\n";
-    dict.inserisci("apple", "fruit");
-    dict.inserisci("car", "vehicle");
-    dict.inserisci("book", "object");
-    dict.inserisci("bike", "vehicle");
-    dict.inserisci("pear", "fruit");
+    diz.inserisci("apple", "fruit");
+    diz.inserisci("car", "vehicle");
+    diz.inserisci("book", "object");
+    diz.inserisci("bike", "vehicle");
+    diz.inserisci("pear", "fruit");
 
     cout << "\nRecupero dei valori:\n";
     string value;
-    if (dict.recupera("apple", value)) {
+    if (diz.recupera("apple", value)) {
         cout << "Value for 'apple': " << value << "\n";
     } else {
         cout << "'apple' not found.\n";
@@ -23,27 +23,27 @@ int main() {
         cout << "'car' not found.\n";
     }
 
-    if (dict.recupera("pen", value)) {
+    if (diz.recupera("pen", value)) {
         cout << "Value for 'pen': " << value << "\n";
     } else {
         cout << "'pen' not found.\n";
     }
 
     cout << "\nVerifica appartenenza:\n";
-    cout << "'bike' presente: " << (dict.appartiene("bike") ? "SI" : "NO") << "\n";
-    cout << "'pen' presente: " << (dict.appartiene("pen") ? "SI" : "NO") << "\n";
+    cout << "'bike' presente: " << (diz.appartiene("bike") ? "SI" : "NO") << "\n";
+    cout << "'pen' presente: " << (diz.appartiene("pen") ? "SI" : "NO") << "\n";
 
     cout << "\nRimozione di 'car':\n";
-    if (dict.rimuovi("car")) {
+    if (diz.rimuovi("car")) {
         cout << "'car' rimossa con successo.\n";
     } else {
         cout << "Errore nella rimozione di 'car'.\n";
     }
 
-    cout << "'car' presente dopo rimozione: " << (dict.appartiene("car") ? "SI" : "NO") << "\n";
+    cout << "'car' presente dopo rimozione: " << (diz.appartiene("car") ? "SI" : "NO") << "\n";
 
     cout << "\nStato finale della tabella hash:\n";
-    dict.stampa();
+    diz.stampa();
 
     return 0;
 }
